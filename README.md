4fdd479019b2df39e85f51dc504017ec06770ae5
Claire Thorington <claire.thorington@gmail.com>
2022-06-10 16:34:44 -0700

    polish existing content for submission

--- README.md
+++ README.md
@@ -26,8 +26,7 @@ This page asks the user for a number and returns a range of numbers from 0 to th
 # _Describe: output(number)_
 
 Test: "It should return an array of numbers from 0 to the user's number"
-Code:
-output(5);
+Code: output(5);
 Expected Output: [0, 1, 2, 3, 4, 5]
 
 ## Setup/Installation Requirements
@@ -38,7 +37,9 @@ Expected Output: [0, 1, 2, 3, 4, 5]
 
 ## Known Bugs
 
-* No known bugs
+* Doesn't yet replace numbers that contain a 1 with "Beep!"
+* Doesn't yet replace numbers that contain a 2 with "Boop!"
+* Doesn't yet replace numbers that contain a 3 with "Won't you be my neighbor?"
 
 ## License
 
--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,9 @@ function output(number) {
   for (let index = 0; index <= parseInt(number); index += 1) {
    resultArray.push(index);
   }
-    return (resultArray + " ");
+    return (resultArray + "");
   }
   
-
-
-
-  // if (noInput(number)) {
-  //   return 0;
-  // }
-  // if (notNumber(number)) {
-  //   return "Not a number!";
-  // }
-  // if (number1 < 0) {
-  //   return "Please use positive numbers!";
-  // }
 
 //UI Logic//
 
@@ -31,12 +19,3 @@ $(document).ready(function(){
     $("#output").html(result);
   });
 });
-
-// function replace(resultArray) {
-//   const modified = result.split(" ");
-//   modified.forEach(function (result) {
-//     if (result.includes(1)) {
-//       modified.splice(index, 1, "Beep!");
-//     }
-//     index++;
-//   });
