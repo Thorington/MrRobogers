1f138c843c5fed1198cf63f35655b094921d024b
Claire Thorington <claire.thorington@gmail.com>
2022-06-16 10:20:30 -0700

    fix final bug

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,14 @@ function output(number) {
     let resultArray = [];
     for (let index = 0; index <= parseInt(number); index += 1) {
   
-      if ([index].toString("").includes("1")) {
-        resultArray.push(" " + "Beep!");
+      if ([index].toString("").includes("3")) {
+        resultArray.push(" " + "Won't you be my neighbor?");
       }
       else if ([index].toString("").includes("2")) {
         resultArray.push(" " + "Boop!");
       }
-      else if ([index].toString("").includes("3")) {
-        resultArray.push(" " + "Won't you be my neighbor?");
+      else if ([index].toString("").includes("1")) {
+        resultArray.push(" " + "Beep!");
       }
       else {resultArray.push(" " + index);
       }
